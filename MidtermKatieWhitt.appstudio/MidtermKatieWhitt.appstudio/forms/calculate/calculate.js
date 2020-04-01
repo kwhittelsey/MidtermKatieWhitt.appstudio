634e6b41aaeb15c144c874eaf50939eb57ad7996
kwhittelsey <60444234+kwhittelsey@users.noreply.github.com>
2020-04-01 15:53:27 -0600

    Add files via upload

---
+++ MidtermKatieWhitt.appstudio/MidtermKatieWhitt.appstudio/forms/calculate/calculate.js
@@ -0,0 +1,13 @@
+/*
+function calcAvgSquared(x,y,z){
+    let calcAvgSquare = ((x + y + z)/3) * (x**2)
+    return calcAvgSquare
+}
+ 
+let num1 = Number(prompt("Enter the first number: "))
+let num2 = Number(prompt("Enter a second number: "))
+let num3 = Number(prompt("Enter a third number: "))
+ 
+let output = calcAvgSquared(num1,num2,num3)
+alert(`The answer is ${output}.`)
+*/
